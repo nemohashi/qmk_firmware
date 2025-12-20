c84b105c5a7df3321215651982492addc9ea5405
nemohashi <nemo.hashimoto@gmail.com>
2025-12-20 22:13:30 +0900

    VIAサポートを有効化 feat: add AL68 keymap and VIA support with custom layouts and keycodes

---
+++ keyboards/yunzii/al68/keymaps/via/config.h
@@ -0,0 +1,12 @@
+/* Copyright 2024 nemohashi
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#pragma once
+
+// VIA プロトコルバージョンを明示的に指定
+#define VIA_EEPROM_LAYOUT_OPTIONS_SIZE 2
