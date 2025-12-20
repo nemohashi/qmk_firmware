ed4ae85d569f30470c63c0de48338510f1c8e477
nemohashi <nemo.hashimoto@gmail.com>
2025-12-20 18:17:54 +0900

    AL68のソースを追加 動作確認OK fix: update keymap for Yunzii AL68 to correct key assignments and add RGB control

--- keyboards/yunzii/al68/keymaps/nikotouch/keymap.c
+++ keyboards/yunzii/al68/keymaps/nikotouch/keymap.c
@@ -49,11 +49,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,    KC_DELETE,
         KC_CAPS,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,                KC_ENT,     KC_PAGE_UP,
         KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSFT,                KC_UP,      KC_PAGE_DOWN,
-        KC_LCTL,    KC_LGUI,    KC_LALT,                            KC_SPC,                             NK_M4,      NK_M2,                              KC_LEFT,    KC_DOWN,    KC_RIGHT    
+        KC_LCTL,    KC_LGUI,    KC_LALT,                            KC_SPC,                             NK_M5,      KC_RCTL,                            KC_LEFT,    KC_DOWN,    KC_RIGHT    
     ),
 
     [NT_FN] = LAYOUT_65_ansi_blocker(
-        KC_GRV,     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     _______,    _______,
+        KC_GRV,     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     RGB_TOG,    _______,
         _______,    KC_USB,     KC_BLE1,    KC_BLE2,    KC_BLE3,    KC_24G,     _______,    _______,    _______,    _______,    _______,    _______,    _______,    RGB_MOD,    _______,
         _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,                RGB_HUI,    _______, 
         _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,                RGB_VAI,    _______,
